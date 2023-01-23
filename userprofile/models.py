620d527220655bd8c1d3819d4ec8abccaa7ff327
Joveth <htivojutiripse14@gmail.com>
2023-01-23 22:22:53 +0800

    Added zip code max_length

--- userprofile/models.py
+++ userprofile/models.py
@@ -32,7 +32,8 @@ class Profile(models.Model):
     zip_code = models.PositiveSmallIntegerField(
         null=True,
         blank=True,
-        validators=[MinValueValidator(1), MaxValueValidator(9999)])
+        validators=[MinValueValidator(1), MaxValueValidator(9999)],
+        max_length=4)
     country = models.CharField(
         max_length=255,
         null=True,
